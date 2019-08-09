12f0e6584e1b91f4ca11fa0c6b15e6bab08e0a06
chhatbarnikita <chhatbarnikita@gmail.com>
2019-08-09 15:27:34 +0530

    Testing for LinkedList insertion

--- src/com/utils/Test.java
+++ src/com/utils/Test.java
@@ -2,13 +2,12 @@ package com.utils;
 
 public class Test {
 	public static void main(String[] args) {
-		Queue<Integer> q = new Queue<>(4);
-		q.enqueue(7);
-		q.enqueue(9);
-		q.enqueue(33);
-		q.enqueue(90);
-		q.dequeue();
-		q.dequeue();
-		System.out.println(q.peek(1));
+		LinkedList<String> list = new LinkedList<String>();
+		list.insertAtEnd("abc");
+		list.insertAtEnd("iui");
+		list.insertAtEnd("iioi");
+		list.insertAtBeginning("90");
+		list.insertAfter("iui", "jio");
+		System.out.println(list);
 	}
 }
