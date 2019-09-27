e934f2aa66eee35b4f1b3634d37287d5aa7a4595
chhatbarnikita <chhatbarnikita@gmail.com>
2019-09-27 16:47:10 +0530

    Completed traversing of the tree (inorder, preorder, postorder)

--- src/com/utils/BinarySearchTree.java
+++ src/com/utils/BinarySearchTree.java
@@ -1,15 +1,83 @@
 package com.utils;
 
-public class BinarySearchTree {
-	class TreeNode<T> {
-		T data;
-		TreeNode<T> left;
-		TreeNode<T> right;
-		
-		public TreeNode(T data) {
-			this.data = data;
-			this.left = null;
-			this.right = null;
-		}
+import java.util.Comparator;
+
+public class BinarySearchTree <T> {
+	private TreeNode<T> root = null;
+	
+	public void insert(T data) {
+		root = add(root, data);		
 	}
+	
+	private TreeNode<T> add(TreeNode<T> current, T data) {
+		if(current == null) {
+			return new TreeNode<T>(data);
+		}
+		
+//		if(curr)
+//			current.left = add(current.left, data);
+//		else if(data > current.data)
+//			current.right = add(current.right, data);
+//		else
+//			return current;
+		return current;
+	}
+	
+	public void preOrder() {
+		preOrderTraversal(root);
+	}
+	
+	public void inorder() {
+		inorderTraversal(root);
+	}
+	
+	public void postOrder() {
+		postOrderTraversal(root);
+	}
+	
+	private void preOrderTraversal(TreeNode<T> root) {
+		if(root == null)
+			return;
+		
+		System.out.print(root.data + " ");
+		preOrderTraversal(root.left);
+		preOrderTraversal(root.right);
+	}
+	
+	private void inorderTraversal(TreeNode<T> root) {
+		if(root == null)
+			return;
+		
+		inorderTraversal(root.left);
+		System.out.print(root.data + " ");
+		inorderTraversal(root.right);
+	}
+	
+	private void postOrderTraversal(TreeNode<T> root) {
+		if(root == null)
+			return;
+		
+		postOrderTraversal(root.left);
+		postOrderTraversal(root.right);
+		System.out.print(root.data + " ");
+	}
+
+}
+
+class TreeNode <T> implements Comparator<T>{
+	T data;
+	TreeNode<T> left;
+	TreeNode<T> right;
+	
+	public TreeNode(T data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+
+	@Override
+	public int compare(T o1, T o2) {
+		return 0;
+	}
+	
 }
--- src/com/utils/Test.java
+++ src/com/utils/Test.java
@@ -2,14 +2,12 @@ package com.utils;
 
 public class Test {
 	public static void main(String[] args) {
-		LinkedList<String> list = new LinkedList<String>();
-		list.insertAtEnd("abc");
-		list.insertAtEnd("iui");
-		list.insertAtEnd("iioi");
-		list.insertAtBeginning("90");
-		list.insertAfter("iui", "jio");
-		System.out.println(list);
-		list.removeAt(3);
-		System.out.println(list);
+		BinarySearchTree node = new BinarySearchTree();
+		
+		node.insert(44);
+		node.insert(33);
+		
+		node.insert(22);
+		
 	}
 }
