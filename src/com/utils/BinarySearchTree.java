21c8c63d21e54dbcf1f199ba339d1a4e94733f1e
chhatbarnikita <chhatbarnikita@gmail.com>
2019-09-28 15:20:05 +0530

    Completion of searching and deleting a node from BST

--- src/com/utils/BinarySearchTree.java
+++ src/com/utils/BinarySearchTree.java
@@ -4,21 +4,15 @@ public class BinarySearchTree <T extends Comparable<T>> {
 	private TreeNode<T> root = null;
 	
 	public void insert(T data) {
-		root = add(root, data);		
+		root = insertRecursive(root, data);		
 	}
 	
-	private TreeNode<T> add(TreeNode<T> current, T data) {
-		if(current == null) {
-			return new TreeNode<T>(data);
-		}
-		
-		if(data.compareTo(current.data) < 0)
-			current.left = add(current.left, data);
-		else if(data.compareTo(current.data) > 0)
-			current.right = add(current.right, data);
-		else
-			return current;
-		return current;
+	public Boolean contains(T data) {
+		return findNode(root, data);
+	}
+	
+	public void delete(T data) {
+		root = deleteRecursive(root, data);
 	}
 	
 	public void preOrder() {
@@ -31,6 +25,63 @@ public class BinarySearchTree <T extends Comparable<T>> {
 	
 	public void postOrder() {
 		postOrderTraversal(root);
+	}
+	
+	private TreeNode<T> insertRecursive(TreeNode<T> current, T data) {
+		if(current == null) {
+			return new TreeNode<T>(data);
+		}
+		
+		if(data.compareTo(current.data) < 0)
+			current.left = insertRecursive(current.left, data);
+		else if(data.compareTo(current.data) > 0)
+			current.right = insertRecursive(current.right, data);
+		else
+			return current;
+		
+		return current;
+	}
+	
+	private Boolean findNode(TreeNode<T> current, T data) {
+		if(current == null)
+			return false;
+		
+		if(data.compareTo(current.data) == 0)
+			return true;
+		
+		return data.compareTo(current.data) < 0 ? findNode(current.left, data) : findNode(current.right, data);
+	}
+	
+	private TreeNode<T> deleteRecursive(TreeNode<T> current, T data){
+		if(current == null)
+			return null;
+		
+		if(data.compareTo(current.data) == 0) {
+			if(current.left == null && current.right == null)
+				return null;
+			
+			if(current.left == null)
+				return current.right;
+			
+			if(current.right == null)
+				return current.left;
+			
+			current.data = findSmallestValue(current.right);
+			current.right = deleteRecursive(current.right, current.data);
+			return current;
+		}
+		
+		else if(data.compareTo(current.data) < 0) {
+			current.left = deleteRecursive(current.left, data);
+			return current;
+		}
+		
+		current.right = deleteRecursive(current.right, data);
+		return current;
+	}
+	
+	private T findSmallestValue(TreeNode<T> node) {
+		return node.left == null ? node.data : findSmallestValue(node.left);
 	}
 	
 	private void preOrderTraversal(TreeNode<T> root) {
--- src/com/utils/Test.java
+++ src/com/utils/Test.java
@@ -2,11 +2,14 @@ package com.utils;
 
 public class Test {
 	public static void main(String[] args) {
-		BinarySearchTree<String> tree = new BinarySearchTree<String>();
-		tree.insert("HA");
-		tree.insert("AI");
-		tree.insert("IO");
-		tree.insert("AA");
-		tree.inorder();
+		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
+		tree.insert(44);
+		tree.insert(22);
+		tree.insert(34);
+		tree.insert(50);
+		tree.insert(59);
+		tree.insert(17);
+		tree.delete(17);
+		tree.inOrder();
 	}
 }
